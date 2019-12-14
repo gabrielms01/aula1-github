1e2c7d5cf929e981bfae695c1a353ed6b2e7f464
Gabriel Menezes <gabriel_oliveiramenezes@yahoo.com.br>
2019-12-14 18:05:30 -0200

    Acrescentada mensagem de bom dia

--- src/testegit/TesteGit.java
+++ src/testegit/TesteGit.java
@@ -16,6 +16,8 @@ public class TesteGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Bom dia!");
     }
     
 }
